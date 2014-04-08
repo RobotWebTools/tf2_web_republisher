87ce711ffce28c5b71929101f9e2d7b90a005fb8
Russell Toris <rctoris@wpi.edu>
2014-04-08 09:14:27 -0400

    cleanup

--- src/tf_web_republisher.cpp
+++ src/tf_web_republisher.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************
  *
- *  Copyright (c) 2009, Willow Garage, Inc.
+ *  Copyright (c) 2014, Willow Garage, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
